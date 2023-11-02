4b40da6cc1fa54db6b193134bed9130ab359c63b
MarkTanashchuk <mark.tanashchuk@gmail.com>
2023-11-02 21:16:51 +0200

    "Initial example project setup"

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+  import '../app.css';
+</script>
+
+<svelte:head>
+  <script src="node_modules/tinymce/tinymce.min.js" referrerpolicy="origin"></script>
+</svelte:head>
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Editor from '$widgets/editor.svelte';
+
+  const text: string = 'Visit kit.svelte.dev to read the documentation';
+</script>
+
+<div class="w-full h-full flex flex-col items-center">
+  <Editor />
+
+  <h1>Welcome to your library project</h1>
+  <p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
+
+  <a class="flex button text-3xl" href="https://kit.svelte.dev">{text}</a>
+</div>
+
+<style>
+  .button {
+    padding: 0.5rem;
+
+    border: 1px solid rgb(0, 0, 0);
+    border-radius: 0.25rem;
+
+    color: rgb(0, 0, 0);
+
+    font-size: 2rem;
+
+    cursor: pointer;
+  }
+</style>
