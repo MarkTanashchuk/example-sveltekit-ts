ce27343bad5fa1a5b9ac93463b145c0bb5214318
MarkTanashchuk <mark.tanashchuk@gmail.com>
2023-12-22 03:44:21 +0200

    feat: initial setup

---
+++ .storybook/main.ts
@@ -0,0 +1,22 @@
+import type { StorybookConfig } from '@storybook/sveltekit';
+
+const config: StorybookConfig = {
+  stories: ['../src/**/*.stories.@(js|jsx|mjs|ts|tsx|svelte)'],
+  addons: [
+    '@storybook/addon-a11y',
+    '@storybook/addon-storysource',
+
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-svelte-csf',
+    '@storybook/addon-interactions'
+  ],
+  framework: {
+    name: '@storybook/sveltekit',
+    options: {}
+  },
+  docs: {
+    autodocs: 'tag'
+  }
+};
+export default config;
