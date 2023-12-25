ba2fe50c7e00df9eea1f570bad72ab4095fe6537
MarkTanashchuk <mark.tanashchuk@gmail.com>
2023-12-25 03:33:01 +0200

    feat: setup mvp navigation components

---
+++ src/shared/ui/navigation/index.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import {
+    Breadcrumb,
+    BreadcrumbItem,
+    Header,
+    SideNav,
+    SideNavItems,
+    SideNavLink,
+    Content
+  } from 'carbon-components-svelte';
+  import Fade from 'carbon-icons-svelte/lib/Fade.svelte';
+
+  const PROJECT_NAME = 'Проєкт';
+  const PLATFORM_NAME = 'Інтерактивне навчання';
+</script>
+
+<Header company={PROJECT_NAME} platformName={PLATFORM_NAME} isSideNavOpen>
+  <Breadcrumb noTrailingSlash>
+    <BreadcrumbItem href="/">Dashboard</BreadcrumbItem>
+    <BreadcrumbItem href="/reports">Annual reports</BreadcrumbItem>
+    <BreadcrumbItem href="/reports/2019" isCurrentPage>2019</BreadcrumbItem>
+  </Breadcrumb>
+</Header>
+
+<SideNav isOpen>
+  <SideNavItems>
+    <SideNavLink icon={Fade} text="Посилання 1" href="/" isSelected />
+  </SideNavItems>
+</SideNav>
+
+<Content>
+  <slot />
+</Content>
---
+++ src/shared/ui/navigation/navigation.stories.svelte
@@ -0,0 +1,18 @@
+<script lang="ts" context="module">
+  import Navigation from './index.svelte';
+
+  export const meta = {
+    title: 'Navigation',
+    component: Navigation
+  };
+</script>
+
+<script lang="ts">
+  import { Story } from '@storybook/addon-svelte-csf';
+</script>
+
+<Story name="Default">
+  <Navigation>
+    <h1>Welcome</h1>
+  </Navigation>
+</Story>
